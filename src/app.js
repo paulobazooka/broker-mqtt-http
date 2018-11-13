8d9c06885d98b6a5843b46f739ebec7dd103d372
Paulo <embrapaprodutor@gmail.com>
2018-11-13 17:19:07 -0200

    criação de banco de dados online no site mLab e configurações de conexão através do pacote mongoose

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@
 
 const express = require('express'); // busca o pacote express
 const bodyParser = require('body-parser'); // importa o pacote para converter requeste em Json
+const mongoose = require('mongoose'); // importa o pacote mongoose
 const mainRoute = require('../routes/main'); // importa a rota principal
 const sensorRoute = require('../routes/sensor-route'); // importa a rota de sensores
 
 const app = express();  // cria uma constante app 
+
+// Conexão com o Banco de dados MongoDb do mLab.com  user:mqtt123  500mb de espaço gratuito
+mongoose.connect('mongodb://user:mqtt123@ds058508.mlab.com:58508/mqtt-server');
 
 // Converter automaticamente o corpo das requisições para Json
 app.use(bodyParser.json());
