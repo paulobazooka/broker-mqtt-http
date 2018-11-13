5a0893cb6c84d73a593bbc68045f9e82561cab62
Paulo <embrapaprodutor@gmail.com>
2018-11-13 16:00:51 -0200

    separação da rota principal do app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
+'use strict'
+
 const express = require('express'); // busca o pacote express
+const bodyParser = require('body-parser'); // importa o pacote para converter requeste em Json
+const mainRoute = require('../routes/main'); // importa a rota principal
+
 const app = express();  // cria uma constante app 
-const router = express.Router(); // cria uma constante de rotas 
 
-// Criação da rota main
-const main = router.get('/', (req, res, next)=>{
-    res.status(200).send({
-        mensagem:'Servidor'
-    });
-});
-
+// Converter automaticamente o corpo das requisições para Json
+app.use(bodyParser.json());
+// condificar as urls
+app.use(bodyParser.urlencoded({extended: false}));
 // declaração de rota onde irá responder
-app.use('/', main);
+app.use('/', mainRoute);
 
 
 // exporta o app
